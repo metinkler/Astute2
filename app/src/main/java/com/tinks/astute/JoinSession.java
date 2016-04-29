e5816ffde5850dd58fce6220b266b1a4683af78b
eobie3 <eobie3@aol.com>
2016-04-29 19:32:55 -0400

    Able to pass information between JoinSession and MainActivity. Should be able to access EditSession. Note that the switch to these activities occurs in CustomListAdapter, NOT MainActivity.

--- app/src/main/java/com/tinks/astute/JoinSession.java
+++ app/src/main/java/com/tinks/astute/JoinSession.java
@@ -1,34 +1,10 @@
 package com.tinks.astute;
 
-import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-
-import android.view.Menu;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
-import android.widget.AdapterView;
-import android.widget.ImageView;
-
-import android.view.View;
-import android.widget.AdapterView.OnItemClickListener;
-import android.text.TextWatcher;
 import android.content.Intent;
-import java.util.ArrayList;
-import android.support.v4.widget.DrawerLayout;
-
-import android.os.PersistableBundle;
-import android.support.v4.view.GravityCompat;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.MenuItem;
-import android.content.res.Configuration;
-import android.widget.Button;
-import android.widget.Filter;
 import android.widget.TextView;
 
 public class JoinSession extends AppCompatActivity {
@@ -46,6 +22,8 @@ public class JoinSession extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Intent intent = getIntent();
+
+        Log.v("JoinSession", "HERE");
 
         location = intent.getStringExtra("location");
         members = intent.getStringExtra("members");
